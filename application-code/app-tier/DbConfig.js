ec6295c74b57fff0f1f7e9a92c4ee26cbe360660
Lennie Nurse <lnursej@gmail.com>
2024-04-23 22:42:46 -0400

    Configured the 3 tier database with test credentials for the app tier.

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+  DB_HOST: "database-1.cluster-c1iugoqw2et9.us-east-1.rds.amazonaws.com",
+  DB_USER: "SL_3tier_Architecture_len_01",
+  DB_PWD: "SL1v3r_01",
+  DB_DATABASE: "webappdb",
 });
